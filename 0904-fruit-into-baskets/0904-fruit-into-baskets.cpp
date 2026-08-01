1a3a977e35e17959a93653ef5a053a527eb43371
Aditya Kulkarni <160615870+adicoolls@users.noreply.github.com>
2026-08-01 16:13:36 +0530

    Time: 38 ms (75.24%), Space: 88.9 MB (7.22%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        map<int,int>mp;
+        int left = 0;
+        int maxLen = 0;
+
+        for(int right = 0; right<fruits.size(); right++){
+            // add current fruit to map
+            mp[fruits[right]]++;
+            //shrink window if more than 2 fruit types;
+            while(mp.size()>2){
+                mp[fruits[left]]--;
+                if(mp[fruits[left]]==0){
+                    mp.erase(fruits[left]);
+                }
+                left++;
+
+            }
+            maxLen = max(maxLen,right-left +1);
+        }
+        return maxLen;
+
+    }
+};
