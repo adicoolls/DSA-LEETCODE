a0a974eed949a6e21c02298db66b89873fa654cf
Aditya Kulkarni <160615870+adicoolls@users.noreply.github.com>
2026-08-12 16:17:15 +0530

    Time: 0 ms (100%), Space: 32.6 MB (63.61%) - LeetHub

---
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int atmostKsum(vector<int>& nums, int goal){
+
+        if(goal < 0){
+            return 0;
+        }
+        int count = 0;
+        int left = 0;
+        unordered_map<int,int>freq;
+        int sum = 0;
+
+        for(int right = 0; right <nums.size(); right++){
+                
+                sum+=nums[right];
+
+                while(sum > goal){
+                    sum -= nums[left];
+                    left++;
+                }
+
+
+            count += right - left + 1;
+            
+
+
+        }
+        return count;
+    }
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return atmostKsum(nums,goal) - atmostKsum(nums,goal-1);
+    }
+};
